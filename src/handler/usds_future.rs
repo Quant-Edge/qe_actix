6d740a68c837a9b92ad74a66895989b139638735
fangyang_gpu <fangyang.jing@hotmail.com>
2025-07-17 00:14:03 +0800

    [Add] usds_future exchange information api

--- src/handler/usds_future.rs
+++ src/handler/usds_future.rs
@@ -6,9 +6,12 @@ use actix_web::web;
 pub fn routes(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::scope("/usds_future")
+            // GET method
             .service(get::account::account_information)
             .service(get::account::account_balance)
+            .service(get::exchange::exchange_information)
             .service(get::position::position_information)
+            // POST method
             .service(post::position::change_position_mode)
             .service(post::kline::kline)
             .service(post::order::new_order),
